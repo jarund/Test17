baf6e3ca483d2906e21a13e2381717a57d24190a
Jeff Rund <jeff.rund@taluserp.com>
2021-02-11 07:02:35 -0500

    web_environment_ribbon

---
+++ web_environment_ribbon/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Web Environment Ribbon",
+    'version': '14.0.1.0.0',
+    'category': 'Web',
+    'author': 'Ketan Kachhela',
+    'website': '',
+    'license': 'AGPL-3',
+    "depends": [
+        'web',
+        ],
+    "data": [
+        'security/ir.model.access.csv',
+        'data/ribbon_data.xml',
+        'view/base_view.xml',
+        ],
+    "auto_install": False,
+    'installable': True
+}
